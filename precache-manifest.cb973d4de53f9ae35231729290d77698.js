4094a67d0cc14bac6cd0cd9b75f89474c7670852
Yurii Adamenko <skyey07@gmail.com>
2020-06-15 22:57:08 +0300

    Updates

---
+++ precache-manifest.cb973d4de53f9ae35231729290d77698.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c1c18e58a2f9bc446e3d8c234b9330",
+    "url": "/react-cv/index.html"
+  },
+  {
+    "revision": "b78469ed488b8a685e53",
+    "url": "/react-cv/static/css/2.b09d5d9e.chunk.css"
+  },
+  {
+    "revision": "d36d0521d7c5ec3d131e",
+    "url": "/react-cv/static/css/main.788b1234.chunk.css"
+  },
+  {
+    "revision": "b78469ed488b8a685e53",
+    "url": "/react-cv/static/js/2.b3e52b1f.chunk.js"
+  },
+  {
+    "revision": "e078d673834ef0c088fe44bab9fcc203",
+    "url": "/react-cv/static/js/2.b3e52b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d36d0521d7c5ec3d131e",
+    "url": "/react-cv/static/js/main.daf21120.chunk.js"
+  },
+  {
+    "revision": "2df592bb566954d414a2",
+    "url": "/react-cv/static/js/runtime-main.64145a43.js"
+  }
+]);
