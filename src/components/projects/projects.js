8a7b85534ff1a068ff0c81e7ae8a5d84e0914e51
Yurii Adamenko <skyey07@gmail.com>
2020-06-18 22:04:43 +0300

    project cards added

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -2,84 +2,109 @@ import React, { Component } from 'react';
 import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 import classes from './projects.module.css'
 
+const cards = [
+  {
+    name: 'ALL',
+    id: 0,
+    urlImage: 'https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg',
+    title: 'Project',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s',
+    github: '',
+    codepen: '',
+    livedemo: ''
+  },
+  {
+    name: 'LATEST',
+    id: 1,
+    urlImage: 'https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg',
+    title: 'Project',
+    text: 'lorem lorem',
+    github: '',
+    codepen: '',
+    livedemo: ''
+  },
+  {
+    name: 'FRONTEND',
+    id: 2,
+    urlImage: 'https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg',
+    title: 'Project',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s',
+    github: '',
+    codepen: '',
+    livedemo: ''
+  },
+  {
+    name: 'REACT',
+    id: 3,
+    urlImage: 'https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg',
+    title: 'Project',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s',
+    github: '',
+    codepen: '',
+    livedemo: ''
+  },
+  {
+    name: 'JS',
+    id: 4,
+    urlImage: 'https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg',
+    title: 'Project',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s',
+    github: '',
+    codepen: '',
+    livedemo: ''
+  }
+];
+
+
+
 class Projects extends Component {
   constructor(props) {
     super(props);
     this.state = { activeTab: 0 };
   }
 
+  filterCards () {}
+
   toggleCategories() {
+
+    const filterCards = cards.filter(card => card.id === this.state.activeTab).map(filteredCard => (
+
+    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+      <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >{filteredCard.title} {filteredCard.id}</CardTitle>
+      <CardText>
+        {filteredCard.text}
+      </CardText>
+      <CardActions border>
+        <Button colored>GitHub</Button>
+        <Button colored>CodePen</Button>
+        <Button colored>Live Demo</Button>
+      </CardActions>
+      <CardMenu style={{color: '#fff'}}>
+        <IconButton name="share" />
+      </CardMenu>
+    </Card>
+    ))
 
     if(this.state.activeTab === 0){
       return(
-        <div className="projects-grid">
-          {/* Project 1 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #1</CardTitle>
-            <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-            </CardText>
-            <CardActions border>
-              <Button colored>GitHub</Button>
-              <Button colored>CodePen</Button>
-              <Button colored>Live Demo</Button>
-            </CardActions>
-            <CardMenu style={{color: '#fff'}}>
-              <IconButton name="share" />
-            </CardMenu>
-          </Card>
-
-          {/* Project 2 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #2</CardTitle>
-            <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-            </CardText>
-            <CardActions border>
-              <Button colored>GitHub</Button>
-              <Button colored>CodePen</Button>
-              <Button colored>Live Demo</Button>
-            </CardActions>
-            <CardMenu style={{color: '#fff'}}>
-              <IconButton name="share" />
-            </CardMenu>
-          </Card>
-
-          {/* Project 3 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #3</CardTitle>
-            <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-            </CardText>
-            <CardActions border>
-              <Button colored>GitHub</Button>
-              <Button colored>CodePen</Button>
-              <Button colored>Live Demo</Button>
-            </CardActions>
-            <CardMenu style={{color: '#fff'}}>
-              <IconButton name="share" />
-            </CardMenu>
-          </Card>
-        </div>
-
-
+          filterCards
       )
     } else if(this.state.activeTab === 1) {
       return (
-        <div><h1>This is Latest</h1></div>
+          filterCards
       )
     } else if(this.state.activeTab === 2) {
       return (
-        <div><h1>This is Front-end</h1></div>
+        filterCards
       )
     } else if(this.state.activeTab === 3) {
       return (
-        <div><h1>This is React</h1></div>
+        filterCards
       )
     }
     else if(this.state.activeTab === 4) {
       return (
-          <div><h1>This is JavaScript</h1></div>
+          filterCards
       )
     }
 
@@ -99,7 +124,11 @@ class Projects extends Component {
         </Tabs>
           <Grid>
             <Cell col={12}>
-              <div className="content">{this.toggleCategories()}</div>
+              <div className="content">
+                <div className="projects-grid">
+                {this.toggleCategories()}
+                </div>
+                </div>
             </Cell>
           </Grid>
       </div>
