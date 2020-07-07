5fdcb414be55dc49c25fb6e1c4238d538c0be62d
Yurii Adamenko <skyey07@gmail.com>
2020-07-08 00:32:06 +0300

    Updates

---
+++ precache-manifest.0454edfa93c5e17dafd83b726abe1b16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4bfc8d6abbe9c64b4237ca4b1dd379",
+    "url": "/react-cv/index.html"
+  },
+  {
+    "revision": "0dd49602db57c7f6b156",
+    "url": "/react-cv/static/css/2.902f3310.chunk.css"
+  },
+  {
+    "revision": "4e7cbccba2c823780266",
+    "url": "/react-cv/static/css/main.8e7af6b6.chunk.css"
+  },
+  {
+    "revision": "0dd49602db57c7f6b156",
+    "url": "/react-cv/static/js/2.d6263372.chunk.js"
+  },
+  {
+    "revision": "e078d673834ef0c088fe44bab9fcc203",
+    "url": "/react-cv/static/js/2.d6263372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7cbccba2c823780266",
+    "url": "/react-cv/static/js/main.ea18776b.chunk.js"
+  },
+  {
+    "revision": "2df592bb566954d414a2",
+    "url": "/react-cv/static/js/runtime-main.64145a43.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-cv/precache-manifest.79671e46ef4ad8269c89ac082b8fe6a0.js"
+  "/react-cv/precache-manifest.0454edfa93c5e17dafd83b726abe1b16.js"
 );
 
 self.addEventListener('message', (event) => {
