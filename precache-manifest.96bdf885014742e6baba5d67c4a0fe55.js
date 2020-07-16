13a806dfb3d910b01f3877380031975e23d2546e
Yurii07 <skyey07@gmail.com>
2020-07-16 13:48:16 +0300

    Updates

---
+++ precache-manifest.96bdf885014742e6baba5d67c4a0fe55.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5661d01415cca65b9cd628875c920b0e",
+    "url": "/react-cv/index.html"
+  },
+  {
+    "revision": "7b47edb1c7b8eaa3fe46",
+    "url": "/react-cv/static/css/2.b09d5d9e.chunk.css"
+  },
+  {
+    "revision": "7d901f29d611f387eefb",
+    "url": "/react-cv/static/css/main.7c0abdb0.chunk.css"
+  },
+  {
+    "revision": "7b47edb1c7b8eaa3fe46",
+    "url": "/react-cv/static/js/2.ca333768.chunk.js"
+  },
+  {
+    "revision": "e078d673834ef0c088fe44bab9fcc203",
+    "url": "/react-cv/static/js/2.ca333768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d901f29d611f387eefb",
+    "url": "/react-cv/static/js/main.bce46aa1.chunk.js"
+  },
+  {
+    "revision": "2df592bb566954d414a2",
+    "url": "/react-cv/static/js/runtime-main.64145a43.js"
+  },
+  {
+    "revision": "95e0aae0c91df7a9071bc83dbe0a64cb",
+    "url": "/react-cv/static/media/api.95e0aae0.JPG"
+  },
+  {
+    "revision": "3ac4c5b4680da8ab7ff4bc3938a06a55",
+    "url": "/react-cv/static/media/bitcoin_bg.3ac4c5b4.jpg"
+  },
+  {
+    "revision": "b47c6e712de506b48b07f813517e485d",
+    "url": "/react-cv/static/media/curriculum-vitae.b47c6e71.png"
+  },
+  {
+    "revision": "e2ba4d11efc729959d853c468d8efe92",
+    "url": "/react-cv/static/media/foodltd.e2ba4d11.jpg"
+  },
+  {
+    "revision": "2a481d9faf9a9751f3923fe1eeb59697",
+    "url": "/react-cv/static/media/game.2a481d9f.png"
+  },
+  {
+    "revision": "92e7041db18a2f32a4e83b5e3a32e34b",
+    "url": "/react-cv/static/media/goodInc.92e7041d.png"
+  },
+  {
+    "revision": "c1342ccdec1d577bbe57cbdd3b8f6638",
+    "url": "/react-cv/static/media/nois-wallpaper-abstract.c1342ccd.jpg"
+  },
+  {
+    "revision": "f19790b8075544cea033f8f9b2f1ffea",
+    "url": "/react-cv/static/media/react-weather.f19790b8.png"
+  },
+  {
+    "revision": "2562620875796ea0d0c94983f9debc62",
+    "url": "/react-cv/static/media/test.25626208.png"
+  },
+  {
+    "revision": "81203842c85997e06d8b6ab00af7d7a9",
+    "url": "/react-cv/static/media/testing.81203842.jpg"
+  },
+  {
+    "revision": "c8065344266364baa67fc5ffe5d4eb99",
+    "url": "/react-cv/static/media/waxm.c8065344.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-cv/precache-manifest.0454edfa93c5e17dafd83b726abe1b16.js"
+  "/react-cv/precache-manifest.96bdf885014742e6baba5d67c4a0fe55.js"
 );
 
 self.addEventListener('message', (event) => {
